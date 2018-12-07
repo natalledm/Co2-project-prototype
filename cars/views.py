226603f028e09bd4e6d198f879c24bd563e06f58
Natzsche <natallemoura7@gmail.com>
2018-12-07 17:07:43 +0100

    posting the changes on models based on the data

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from .models import Cars
 
 def home(request):
     return HttpResponse('Hello, world!')
+
+def filterByBrand(request, brand):
+    return Cars.objects.filter(brand__startswith=brand)[:4]
